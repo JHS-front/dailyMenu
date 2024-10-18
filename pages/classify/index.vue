72dc5c32788b3ccbd868c631dbf30429649dc58a
jinhaisheng <1054703944@qq.com>
2024-10-18 17:49:00 +0800

    调整

--- pages/classify/index.vue
+++ pages/classify/index.vue
@@ -2,9 +2,26 @@
 	<view class="body">
 		<view class="head">全部分类</view>
 		<view class="main">
-			<u--input placeholder="输入关键词" :shape="circle" prefixIcon="search" prefixIconStyle="font-size: 22px;color: #909399"></u--input>
+			<u--input placeholder="输入关键词" v-model="keyword" @input="handleInput" :shape="circle" prefixIcon="search" prefixIconStyle="font-size: 22px;color: #909399"></u--input>
 			<view class="mainMenu">
-
+				<view class="left">
+					<view class="leftFor" v-for="(item,index) in classifyList" :key="index" @click="selectItem(item,index)" :class="{ selected: selectedIndex === index }">
+						<view class="leftItem">{{ item.name }}</view>
+					</view>
+				</view>
+				<view class="right">
+					<view class="rightFor" v-for="(item,index) in filterMainList" :key="index">
+						<view class="rightItem">
+							<image :src="item.imgUrl" class="cellheadImg"></image>
+                			<view class="cellText">
+								<view class="cellheadText">{{ item.headText }}</view>
+								<view class="cellsubText">
+									<span class="overflow-eps">原料：{{ item.subText }}</span>
+								</view>
+							</view>
+						</view>
+					</view>
+				</view>
 			</view>
 		</view>
 	</view>
@@ -12,16 +29,75 @@
 </template>
 
 <script>
+	import { foodLists } from '../../utils/foodMock.js'; // mock数据
 	export default {
 		data() {
 			return {
-				title: 'Hello 分类'
+				classifyList:[
+					{
+						id:"1",
+						name:"蔬菜类",
+						imgUrl:"../../static/index/vegetable.png",
+					},{
+						id:"2",
+						name:"汤类",
+						imgUrl:"../../static/index/soup.png",
+					},{
+						id:"3",
+						name:"鱼类",
+						imgUrl:"../../static/index/fish.png",
+					},{
+						id:"4",
+						name:"肉类",
+						imgUrl:"../../static/index/meat.png",
+					},{
+						id:"5",
+						name:"海鲜类",
+						imgUrl:"../../static/index/seafood.png",
+					},{
+						id:"6",
+						name:"主食类",
+						imgUrl:"../../static/index/food.png",
+					},{
+						id:"7",
+						name:"甜品类",
+						imgUrl:"../../static/index/dessert.png",
+					},{
+						id:"8",
+						name:"豆制品类",
+						imgUrl:"../../static/index/bean.png",
+					}
+				],
+				keyword:"",
+				mainList:[],
+				filterMainList:[],
+				selectedIndex:null
 			}
 		},
-		onLoad() {
+		computed: {
 
 		},
+		onLoad() {
+			console.log(foodLists,'foodLists');
+			this.keyword = ""
+			this.selectedIndex = 0; // 默认选中第一个
+			this.mainList = foodLists.filter(filterItem => filterItem.type === this.classifyList[0].name); // 默认显示第一个分类的数据
+			this.filterMainList = foodLists.filter(filterItem => filterItem.type === this.classifyList[0].name);
+		},
 		methods: {
+			handleInput(e) {
+				this.filterMainList = this.mainList.filter(item => {
+					// 这里使用了toLowerCase来忽略大小写的差异
+					return item.headText.toLowerCase().includes(this.keyword.toLowerCase());
+				});
+			},
+			selectItem(item,index) {
+				this.selectedIndex = index; // 更新选中的 index
+				// 使用 filter 方法筛选 foodLists
+				this.mainList = foodLists.filter(filterItem => filterItem.type === item.name);
+				this.filterMainList = foodLists.filter(filterItem => filterItem.type === item.name);
+				this.keyword = ""
+			},
 
 		}
 	}
@@ -29,7 +105,7 @@
 
 <style lang="scss" scoped>
 .body {
-	background-color: rgba(246, 247, 251, 1);
+	background-color: #ffffff;
 	height: 100vh;
 	width: 100%;
 	.head{        
@@ -55,6 +131,90 @@
 		}
 		.mainMenu{
 			margin-top: 20rpx;
+			display: flex;
+			.left{
+				display: flex;
+				flex-direction: column;
+				width: 20%;
+				height: 60vh;
+    			background: #F1F2F4;
+				.leftFor{
+					display: flex;
+					flex-direction: column;
+					height: 96rpx;
+					justify-content: center;
+					box-sizing: border-box;
+					.leftItem{
+						padding-left: 26rpx;
+						box-sizing: border-box;
+						font-weight: 400;
+						font-size: 28rpx;
+						color: rgba(36,38,41,0.65);
+					}
+				}
+				.leftFor.selected {
+					background-color: #FFFFFF;
+					.leftItem{
+						padding-left: 23rpx;
+						border-left: 3px solid #0052d9;
+						font-weight: 600;
+						color: #0052d9;
+					}
+				}
+			}
+			.right{
+				background: #FFFFFF;
+				padding: 0 20rpx;
+				box-sizing: border-box;
+				flex: 1;
+				height: 80vh;
+    			overflow: auto;
+				.rightFor{
+					display: flex;
+					flex-direction: column;
+					height: 200rpx;
+					justify-content: center;
+					margin-bottom: 20rpx;
+					background: #F1F2F4;
+    				border-radius: 16rpx;
+					.rightItem{
+						display: flex;
+						align-items: center;
+						.cellheadImg{
+							margin-right: 20rpx;
+							width: 36%;
+							height: 200rpx;
+						}
+						.cellText{
+							flex: 1;
+							display: flex;
+							flex-direction: column;
+							justify-content: space-around;
+							height: 100%;
+							padding: 20rpx 0;
+							box-sizing: border-box;
+							.cellheadText{
+								font-weight: 600;
+								font-size: 30rpx;
+								color: #242629;
+							}
+							.cellsubText{
+								font-weight: 500;
+								font-size: 24rpx;
+								color: rgba(36,38,41,0.45);
+								.overflow-eps{
+									overflow: hidden;
+    								text-overflow: ellipsis;
+									display:-webkit-box;
+									-webkit-line-clamp: 2; /*第几行裁剪*/
+									-webkit-box-orient:vertical;
+								}
+							}
+						}
+
+					}
+				}
+			}
 		}
 	}
 }
--- pages/my/weal.vue
+++ pages/my/weal.vue
@@ -1,7 +1,7 @@
 <template>
   <view class="myContainer">
-    <video :src="videoUrl" controls style="width: 100%;"></video>
-    <img :src="imgUrl" alt="">
+    <!-- <video :src="videoUrl" controls style="width: 100%;"></video> -->
+    <image :src="imgUrl" mode="aspectFit" alt="" class="aboutusImage">
   </view>
 </template>
 
@@ -12,7 +12,7 @@ export default {
   data() {
     return {
       videoUrl: 'https://vwecam.gtimg.com/1006_f0a5d88c884e490d8e38358fbd9accbb.f0.mp4?ptype=http&vkey=E4B82B446CF255E316A35051E2325CE6D88646E8C0702F70F8756B7C632C1DB2D25891AC6475CDBDA1B86E7EA4DC95A88D0D23D2289B429A&sdtfrom=v1000&owner=0&chgtype=1010&vuin=1054703944',
-      imgUrl:'https://photocq.photo.store.qq.com/psc?/V12FD0Ht1Yk2Wh/OO0GOkEvd.dIewPrYqD0DqqZLYYWgbjMGpiuLa9MT*VWLrWK3oX*6CeVrqNDMiqfWgLq2JD63.SuMF3vyZs6Qg!!/b&bo=OAQGBgAAAAARBww!&rf=viewer_4'
+      imgUrl:'http://photocq.photo.store.qq.com/psc?/V12FD0Ht250yMy/OO0GOkEvd.dIewPrYqD0Dhc1zWDIz2zm6B4E*O6Am176iiEvQp*WezQkEShqvGQeCdvH2amp.VwPnyZXbtgumQ!!/b&bo=0AIABQAAAAAFB*M!&rf=viewer_4'
     }
   },
   methods:{
@@ -29,5 +29,10 @@ export default {
   background: #F6F7FB;
   height: 100vh;
 	width: 100%;
+  .aboutusImage{
+    width: 100%;
+    height: 100%; /* 使用百分比或者rpx单位 */
+    display: block;
+  }
 }
 </style>
